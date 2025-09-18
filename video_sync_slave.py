ce3fdfb5cef5ffa93870a872610c176af20cd009
Jonas Pöller <jonaspoller@googlemail.com>
2025-09-18 12:02:17 +0200

    Update video_sync_slave.py
    bugfixes

--- video_sync_slave.py
+++ video_sync_slave.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-# ==================================================================================
-# Synchronized Video Playback - Slave Node (FINALE, SCALE-FIX)
-#
-# LÖSUNG: Verwendet player.video_set_scale(0), um das Bild aggressiv
-#         auf Vollbild zu zwingen.
-# ==================================================================================
-import vlc, socket, time, json, configparser, os
+import vlc, socket, time, json, configparser, os, threading
 
 class VideoSyncSlave:
     def __init__(self, config_file='sync_config.ini'):
@@ -26,27 +20,40 @@ class VideoSyncSlave:
         self.sock.bind(('', self.sync_port))
         
         self.running = True
+        self.last_sync_time = time.time()
 
     def handle_command(self, message):
         if message.get('master_ip') != self.master_ip: return
+        self.last_sync_time = time.time()
         
         command = message.get('command')
         
         if command == 'stop':
-            self.player.set_media(self.black_media)
-            self.player.play()
+            self.player.set_media(self.black_media); self.player.play()
         elif command == 'load':
+            video_path = message.get('data', {}).get('video_path')
+            if video_path:
+                self.media = self.instance.media_new(video_path)
+        elif command == 'prepare':
             video_path = message.get('data', {}).get('video_path')
             if video_path and (not self.media or self.media.get_mrl() != video_path):
                 self.media = self.instance.media_new(video_path)
-        elif command == 'prepare':
+                if self.media:
+                    self.media.parse()
+                    if self.media.get_duration() == -1:
+                        print(f"ERROR: Video file '{video_path}' is missing or corrupt.")
+                        self.media = None
+            
             if self.media:
                 self.player.set_media(self.media)
-                self.player.video_set_scale(0)
                 self.player.play()
                 time.sleep(0.2)
                 self.player.pause()
                 self.player.set_position(0)
+            else:
+                self.player.set_media(self.black_media)
+                self.player.play()
+
         elif command == 'play':
             self.player.play()
 
@@ -58,9 +65,23 @@ class VideoSyncSlave:
             except Exception:
                 pass
 
+    def master_watchdog(self):
+        while self.running:
+            time.sleep(2)
+            if time.time() - self.last_sync_time > 5:
+                print("Master signal lost. Reverting to black screen.")
+                self.player.set_media(self.black_media)
+                self.player.play()
+                self.last_sync_time = time.time()
+
     def start(self):
+        print("--- Video Sync Slave (Hardened) ---")
         self.player.set_media(self.black_media)
         self.player.play()
+        print("Waiting for master commands...")
+        
+        watchdog_thread = threading.Thread(target=self.master_watchdog, daemon=True)
+        watchdog_thread.start()
         
         try:
             self.listen_for_commands()
@@ -71,6 +92,7 @@ class VideoSyncSlave:
         self.running = False
         self.player.stop()
         self.sock.close()
+        print("Slave stopped.")
 
 if __name__ == "__main__":
     slave = VideoSyncSlave()
